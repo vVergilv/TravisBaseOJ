4921cea8b6192bb763ed54f9c84289d59744a8e7
vVergilv <1791184666@qq.com>
2021-04-24 16:01:37 +0000

    demo

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/user/bin/env bash
+
+for file in ./*.sh;do
+    if [[ $file =~ $0 ]];then
+        continue
+    fi
+    printf "================== %s ==================\n" "$file"
+    bash "$file"
+done
